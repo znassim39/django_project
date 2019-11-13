ae12875681afc0c171e9be7a743d58cd9f347a12
znassim39 <zehri.n2013@gmail.com>
2019-11-13 13:49:07 +0100

    ajout du modèle Article pour NewApp

--- NewApp/models.py
+++ NewApp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=42)
+    body = models.TextField(null=True)
+    date = models.DateTimeField(default=timezone.now, verbose_name="publishing date")
+
+    class Meta:
+        verbose_name = 'article'
+        ordering = ['date']
+
+    def __str__(self):
+        return self.title
