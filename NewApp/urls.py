fa84f413b3a492a002126e63e7c06dc8e4f18d48
znassim39 <zehri.n2013@gmail.com>
2019-11-15 23:19:15 +0100

    Ajout des formulaires et les views addArticle et addComment

--- NewApp/urls.py
+++ NewApp/urls.py
@@ -6,4 +6,6 @@ app_name = 'NewApp'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:article_id>', views.showArticle, name='article'),
+    path('add', views.addArticle, name='add'),
+    path('addComment', views.addComment, name='addComment'),
 ]
--- NewApp/views.py
+++ NewApp/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from NewApp.models import Article, Comment
+from .forms import ArticleForm, CommentForm
 
 def index(request):
     articles = Article.objects.all()
     return render(request, 'NewApp/accueil.html', {'articles': articles})
 
 def showArticle(request, article_id):
-    article = Article.objects.get(pk=article_id)
+    article = get_object_or_404(Article, pk=article_id)
     comments = article.comment_set.all()
+
+
     return render(request, 'NewApp/article.html', {'article': article, 'comments': comments})
+
+def addArticle(request):
+
+    form = ArticleForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+
+    return render(request, 'NewApp/add.html', locals())
+
+def addComment(request):
+
+    form = CommentForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+
+    return render(request, 'NewApp/addComment.html', locals())
