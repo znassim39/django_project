9766c2e8384e200222fe770213cdbc43ceb653d2
nzehri <zehri.n2013@gmail.com>
2020-11-08 21:31:35 +0100

    ok

--- NewApp/views.py
+++ NewApp/views.py
@@ -5,6 +5,7 @@ from .forms import ArticleForm, CommentForm
 
 def index(request):
     articles = Article.objects.all()
+    print(articles)
     return render(request, 'NewApp/accueil.html', {'articles': articles})
 
 def showArticle(request, article_id):
